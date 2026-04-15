eb1c76f9961529de458b2a77c6f296af634176f9
Salah <2169461@cegepmv.ca>
2026-04-15 11:49:35 -0400

    feature : Afficher Calendrier ajout de components Calendar et de calendar.css

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,78 @@
+﻿import { useState } from "react";
+import "../styles/calendar.css";
+
+export default function Calendar() {
+  const [date, setDate] = useState(new Date());
+
+  const today = new Date();
+
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const weekDays = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"];
+
+  const monthLabel = date.toLocaleDateString("fr-CA", {
+    month: "long",
+    year: "numeric",
+  });
+
+  const prevMonth = () => setDate(new Date(year, month - 1, 1));
+  const nextMonth = () => setDate(new Date(year, month + 1, 1));
+
+  const getDays = () => {
+    const firstDay = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const daysNumbers = [];
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      daysNumbers.push(i);
+    }
+
+    return [
+      ...Array(firstDay).fill(null),
+      ...daysNumbers,
+    ];
+  };
+
+  const isToday = (day: number) => {
+    return (
+      day === today.getDate() &&
+      month === today.getMonth() &&
+      year === today.getFullYear()
+    );
+  };
+
+  const days = getDays();
+
+  return (
+    <div className="calendar-container">
+      <div className="calendar-header">
+        <button onClick={prevMonth}>‹</button>
+        <h2>{monthLabel}</h2>
+        <button onClick={nextMonth}>›</button>
+      </div>
+
+      <div className="calendar-grid">
+        {weekDays.map((day) => (
+          <div key={day} className="header-day">
+            {day}
+          </div>
+        ))}
+
+        {days.map((day, index) =>
+          day ? (
+            <div
+              key={day}
+              className={`day ${isToday(day) ? "today" : ""}`}
+            >
+              {day}
+            </div>
+          ) : (
+            <div key={index} />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import styles from "../styles/HomePage.module.css";
+import Calendar from "../components/Calendar";
 
 export default function HomePage() {
   const { user, logout } = useAuth();
@@ -28,7 +29,9 @@ export default function HomePage() {
       </header>
 
       <main className={styles.main}>
-        <h1>Bonjour, {user!.email} 👋</h1>
+        <h1>Bonjour, {user!.email} </h1>
+        <Calendar />
+        
       </main>
     </div>
   );
